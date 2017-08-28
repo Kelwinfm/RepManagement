86b09e434807ad629a716e5ed8bcee8bc3249504
kelwi <kelwi@DESKTOP-436FRHC>
2017-08-28 15:14:41 -0300

    Classe Contas

---
+++ RepManagement/src/classes/Contas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author kelwin
+ */
+public class Contas {
+    private float valor;
+    private String descricao;
+
+    public Contas(float valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
