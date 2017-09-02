f14e8d229c454a1b355c9d427f80142b64beae94
kelwi <kelwi@DESKTOP-436FRHC>
2017-09-02 16:35:47 -0300

    Principais classes reformuladas de acordo com previo diagrama de classe

--- RepManagement/src/classes/Moradores.java
+++ RepManagement/src/classes/Moradores.java
@@ -10,8 +10,21 @@ package classes;
  * @author kelwin
  */
 public class Moradores {
-    private String Nome;
-    private String Apelido;
-    private int Idade;
-    private float saldo_devedor_reserva;
+    private String nome;
+    private String rg;
+    private String contato;
+    private float saldoDevedor;
+    private String dataEntrada;
+    
+    public void cadastrarMorador(){
+        
+    }
+    public void listarMoradores(){
+        //lista todos os moradores com seus atributos
+    }
+    public int quantMoradores(){
+        //calcula quantidade de moradores na casa
+        int qtd = 0;
+        return qtd;
+    }
 }
